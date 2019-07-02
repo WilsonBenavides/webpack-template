be36d1a825a10dac5744b216613b755bf8ce2f41
ing wilson <ing.wilson.benavides@gmail.com>
2019-07-02 11:36:49 -0500

    8 circulos en la pantalla igualmente espaciados

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import data from '../data.json'
 import suma from './suma'
 import utils from './utils'
 
-//const profesor = 'Jonathan MirCha'
+//const profesor = 'Wilson Benavides'
 
 //console.log('Hola mundo Webpack, sin configuración')
 //console.log(`Hola ${profesor}`)
@@ -29,8 +29,8 @@ console.log(data)
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth 
+canvas.height = innerHeight * 0.65
 
 const mouse = {
     x: innerWidth / 2,
@@ -53,14 +53,14 @@ addEventListener('resize', () => {
 })
 
 // Objects
-function Object(x, y, radius, color) {
+function TicketCircle(x, y, radius, color) {
     this.x = x
     this.y = y
     this.radius = radius
     this.color = color
 }
 
-Object.prototype.draw = function() {
+TicketCircle.prototype.draw = function() {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
     c.fillStyle = this.color
@@ -68,29 +68,32 @@ Object.prototype.draw = function() {
     c.closePath()
 }
 
-Object.prototype.update = function() {
+TicketCircle.prototype.update = function() {
     this.draw()
 }
 
 // Implementation
-let objects
+let circles
 function init() {
-    objects = []
+    circles = []
 
-    for (let i = 0; i < 400; i++) {
-        // objects.push()
-    }
+    let radius = canvas.width / 32
+    let y = radius
+    for (let i = 0; i < 8; i++) {
+    	let wdth = (canvas.width / 8) - (radius / 3.5)
+    	let x = (wdth  * (i + 1)) - wdth + (3 * radius)
+        circles.push(new TicketCircle(x, y, radius, 'blue'))        
+    }           
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
-
-    c.fillText('HTML CANVAS', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    //  object.update()
-    // })
+    
+    circles.forEach(circle => {
+    	circle.update()
+    })
 }
 
 init()
