46221488f5dbca617b4b0508cb7f75362ce16c8f
ing wilson <ing.wilson.benavides@gmail.com>
2019-07-02 15:30:10 -0500

    circulos blancos dibujados, cuando el mouse pasa encima de ellos el borde cambia de color

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,24 @@
+    
+import * as util from './utils'
 import style from '../css/style.css'
 import sass from '../scss/main.scss'
 import logo from '../img/webpack-logo.svg'
 import data from '../data.json'
-import suma from './suma'
-import utils from './utils'
+import { suma } from './suma'    
 
-//const profesor = 'Wilson Benavides'
+
+
+const profesor = 'Wilson Benavides'
 
 //console.log('Hola mundo Webpack, sin configuración')
 //console.log(`Hola ${profesor}`)
-//console.log(suma(5, 3))
+console.log(util.randomIntFromRange(3, 5))
+//console.log(randomIntFromRange(0, 5))
 
-//const d = document,
+//const d = document
   //canvas = d.getElementById('canvas')
   //h1 = d.createElement('h1'),
-  //img = d.createElement('img')
+  //const img = d.createElement('img')
 
 //h1.textContent = 'Hola Mundo con Webpack }:)'
 //img.src = logo
@@ -46,8 +50,8 @@ addEventListener('mousemove', event => {
 })
 
 addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
+    canvas.width = innerWidth 
+	canvas.height = innerHeight * 0.65
 
     init()
 })
@@ -63,26 +67,38 @@ function TicketCircle(x, y, radius, color) {
 TicketCircle.prototype.draw = function() {
     c.beginPath()
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
+    c.fillStyle = 'white'
     c.fill()
+    c.strokeStyle = this.color
+    c.stroke()
     c.closePath()
 }
 
 TicketCircle.prototype.update = function() {
     this.draw()
+
+    if (util.distance(mouse.x, mouse.y, this.x, this.y) < this.radius) {
+        console.log('collided')
+        this.color = 'red'
+    } else {
+        this.color = 'white'        
+    }   
 }
 
 // Implementation
 let circles
+//let circleMouse
 function init() {
-    circles = []
+    circles = []    
+    //circleMouse = new TicketCircle(100, 100, 30, 'green')
 
     let radius = canvas.width / 32
     let y = radius
     for (let i = 0; i < 8; i++) {
-    	let wdth = (canvas.width / 8) - (radius / 3.5)
-    	let x = (wdth  * (i + 1)) - wdth + (3 * radius)
-        circles.push(new TicketCircle(x, y, radius, 'blue'))        
+    	let wdth = (canvas.width / 7.48)
+    	let x = (wdth  * (i + 1)) - wdth + radius
+        circles.push(new TicketCircle(x, y, radius, 'white'))        
+        circles.push(new TicketCircle(x, canvas.height - y, radius, 'white'))        
     }           
 }
 
@@ -90,7 +106,11 @@ function init() {
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
-    
+        
+    // circleMouse.x = mouse.x
+    // circleMouse.y = mouse.y
+    // circleMouse.update()
+
     circles.forEach(circle => {
     	circle.update()
     })
--- src/js/suma.js
+++ src/js/suma.js
@@ -1,1 +1,5 @@
-export const suma = (a, b) => a + b
+export function randomIntFromRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+//export const suma = (a, b) => a + b
